eeed724b745417f6fd1f9c41874aef7a8954c626
ViceRabbit <105757172+ViceRabbit@users.noreply.github.com>
2022-05-22 13:44:48 -0400

    Create mcserver.py

---
+++ py-files/mcserver.py
@@ -0,0 +1,14 @@
+import minestat
+
+# Below is an example using the MineStat class.
+# If server is offline, other instance members will be None.
+ms = minestat.MineStat('TheRetards.ploudos.me', 25565)
+print('Minecraft server status of %s on port %d:' % (ms.address, ms.port))
+if ms.online:
+  print('Server is online running version %s with %s out of %s players.' % (ms.version, ms.current_players, ms.max_players))
+  print('Message of the day: %s' % ms.motd)
+  print('Message of the day without formatting: %s' % ms.stripped_motd)
+  print('Latency: %sms' % ms.latency)
+  print('Connected using protocol: %s' % ms.slp_protocol)
+else:
+  print('Server is offline!')
